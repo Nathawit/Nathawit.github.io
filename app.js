34319c52fe1140dc8bcd86aad70ae464b39eb384
Nathawit <58380791+Nathawit@users.noreply.github.com>
2023-01-01 15:12:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function Approach(e) {
 
 function Fever(e) {
 
-	const Duration = document.querySelector('input[name="Duration"]'); 
+	const Duration = document.querySelector('input[name="Duration"]').value; 
     var AHTR = document.querySelectorAll('input[name^="AHTR"]');
 	
     // Initialize a count to 0
